479073ce86e2ae917928093715a6d0c92645053c
Jessy Li <jessy413@ucla.edu>
2022-10-14 13:50:31 -0700

    correct piping

--- pipe.c
+++ pipe.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
 	if(argc <= 1)
 	{
-		printf("too few arguments, exit with exit status %d\n", EINVAL);
+		perror("too few arguments\n");
 		exit(EINVAL);
 	}
 	int prev_pipe_fd = 0;
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		int pipe_fd[2];
 		if(pipe(pipe_fd)==-1)
 		{	
-			printf("failed pipe, exit with exit status %d\n", ESTRPIPE);
+			perror("failed pipe, exit with exit status %d\n");
 			exit(ESTRPIPE);
 		}
 		int return_code = fork();
@@ -32,29 +32,37 @@ int main(int argc, char *argv[])
 			if(i!=argc-1)
 				dup2(pipe_fd[1],STDOUT_FILENO);
 			
-			//close(prev_pipe_fd);	
-			//close(pipe_fd[1]);
+			close(pipe_fd[1]);
 			close(pipe_fd[0]);			
 			execlp(argv[i],argv[i],NULL);	
-			printf("failed execution of command %s\n", argv[i]);
+			perror("failed execution of command");
 			exit(EINVAL);
 		}
 		else if(return_code > 0)
 		{
 			int pid = return_code;
 			int status = 0;
-			waitpid(pid, &status, 0);
+			if(waitpid(pid, &status, 0)==-1)
+			{
+				exit(EXIT_FAILURE);
+			}
 			if(WIFEXITED(status))
 			{
 				exit_status = WEXITSTATUS(status);
 				if(exit_status != 0)
 					exit(exit_status);
 			}
-			//close(prev_pipe_fd);
-			prev_pipe_fd = pipe_fd[0];
-			//close(pipe_fd[0]);
+			
+			if(i!=1)
+			{
+				close(prev_pipe_fd);
+				prev_pipe_fd = pipe_fd[0];
+			}
+			else
+				prev_pipe_fd = pipe_fd[0];
 			close(pipe_fd[1]);
-
+			if(i==argc-1)
+				close(pipe_fd[0]);
 	
 		}
 		else
